4394a74ea9737f2c2e031918df13d368a185d7e7
developpeur.olive as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-05-02 10:05:02 +0200

    Core Cata Todo List - initial xcode project

---
+++ Core Data Todo List/Core Data Todo List/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Core Data Todo List
+//
+//  Created by olivier geiger on 02/05/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Core Data Todo List/Core Data Todo List/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Core Data Todo List
+//
+//  Created by olivier geiger on 02/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
